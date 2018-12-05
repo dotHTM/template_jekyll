986a3b0c719bba97b3b8b69acb8e2b80da742783
Mike Cramer <mcramer@austin.utexas.edu>
2018-12-05 17:58:44 -0600

    Let's keep this out of the "main"stream, okay?

--- get_template.sh
+++ get_template.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 # get_template.sh
 
+read -p "What do you want to call it? > " projectName
 
-main(){
-    read -p "What do you want to call it? > " projectName
+if [[ -z $projectName ]]; then
+    projectName='sample_template'
+fi
 
-    if [[ -z $projectName ]]; then
-        projectName='sample_template'
+if [[ -n $projectName ]]; then
+    git clone https://github.com/dotHTM/template_jekyll.git "$projectName"
+fi
+
+if [[ -e "$projectName/.git" ]]; then
+    cd "$projectName"
+    rm -rf .git
+    rm get_template.sh
+    git init
+    git add *
+
+    # Open in Editor
+    editor=''
+    if [[ -e `which subl` ]]; then
+        editor='subl'
     fi
-    
-    if [[ -n $projectName ]]; then
-        git clone https://github.com/dotHTM/template_jekyll.git "$projectName"
+
+    if [[ -n $editor ]]; then
+        $editor ./
+        $editor ./README.md
     fi
 
-    if [[ -e "$projectName/.git" ]]; then
-        cd "$projectName"
-        rm -rf .git
-        rm get_template.sh
-        git init
-        git add *
-
-        # Open in Editor
-        editor=''
-        if [[ -e `which subl` ]]; then
-            editor='subl'
-        fi
-
-        if [[ -n $editor ]]; then
-            $editor ./
-            $editor ./README.md
-        fi
-
-        # Open in Git helper
-        gitHelper=''
-        if [[ -e `which smerge` ]]; then
-            gitHelper='smerge'
-        fi
-
-        if [[ -n $gitHelper ]]; then
-            $gitHelper ./
-        fi
+    # Open in Git helper
+    gitHelper=''
+    if [[ -e `which smerge` ]]; then
+        gitHelper='smerge'
     fi
-}
 
-main
+    if [[ -n $gitHelper ]]; then
+        $gitHelper ./
+    fi
+fi
