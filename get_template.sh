e2f41e8d79fe58379447eceae880580c6b78933e
Mike Cramer <mcramer@austin.utexas.edu>
2018-12-05 17:24:22 -0600

    Open in editor

--- get_template.sh
+++ get_template.sh
@@ -7,4 +7,29 @@ rm -rf .git
 rm get_template.sh
 git init
 git add *
-git commit -m "init"
+
+# --- # --- # --- # --- # --- # --- # --- # --- 
+# Open in Editor
+
+editor=''
+if [[ -e `which subl` ]]; then
+    editor='subl'
+fi
+
+if [[ -n $editor ]]; then
+    $editor ./
+    $editor ./README.md
+fi
+
+
+# --- # --- # --- # --- # --- # --- # --- # --- 
+# Open in Git helper
+
+gitHelper=''
+if [[ -e `which smerge` ]]; then
+    gitHelper='smerge'
+fi
+
+if [[ -n $gitHelper ]]; then
+    $gitHelper ./
+fi
