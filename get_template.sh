bfbad9876b3ab85339cbc86685009ac3857de59e
Mike Cramer <mcramer@austin.utexas.edu>
2018-12-05 17:53:34 -0600

    Func-y Town?

--- get_template.sh
+++ get_template.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 # get_template.sh
 
-read -p "What do you want to call it? > " projectName
 
-if [[ -z $projectName ]]; then
-    projectName='sample_template'
-fi
+main(){
+    read -p "What do you want to call it? > " projectName
+
+    if [[ -z $projectName ]]; then
+        projectName='sample_template'
+    fi
     
-if [[ -n $projectName ]]; then
-    git clone https://github.com/dotHTM/template_jekyll.git "$projectName"
-fi
-
-if [[ -e "$projectName/.git" ]]; then
-    #statements
-    cd "$projectName"
-    rm -rf .git
-    rm get_template.sh
-    git init
-    git add *
-
-    # --- # --- # --- # --- # --- # --- # --- # --- 
-    # Open in Editor
-
-    editor=''
-    if [[ -e `which subl` ]]; then
-        editor='subl'
+    if [[ -n $projectName ]]; then
+        git clone https://github.com/dotHTM/template_jekyll.git "$projectName"
     fi
 
-    if [[ -n $editor ]]; then
-        $editor ./
-        $editor ./README.md
+    if [[ -e "$projectName/.git" ]]; then
+        cd "$projectName"
+        rm -rf .git
+        rm get_template.sh
+        git init
+        git add *
+
+        # Open in Editor
+        editor=''
+        if [[ -e `which subl` ]]; then
+            editor='subl'
+        fi
+
+        if [[ -n $editor ]]; then
+            $editor ./
+            $editor ./README.md
+        fi
+
+        # Open in Git helper
+        gitHelper=''
+        if [[ -e `which smerge` ]]; then
+            gitHelper='smerge'
+        fi
+
+        if [[ -n $gitHelper ]]; then
+            $gitHelper ./
+        fi
     fi
+}
 
-    # --- # --- # --- # --- # --- # --- # --- # --- 
-    # Open in Git helper
-
-    gitHelper=''
-    if [[ -e `which smerge` ]]; then
-        gitHelper='smerge'
-    fi
-
-    if [[ -n $gitHelper ]]; then
-        $gitHelper ./
-    fi
-fi
-
+main
